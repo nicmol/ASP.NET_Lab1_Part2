76fef677c6ea9d15817c1edde933fffb437e563e
Nicolette <molitorn@my.lanecc.edu>
2019-10-10 22:09:28 -0700

    working on models from domain model

---
+++ Potlucky/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Potlucky.Models
+{
+    public class Message
+    {
+        public string MessageText { get; set; }
+        public User Sender { get; set; }
+    }
+}
---
+++ Potlucky/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Potlucky.Models
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        private List<Message> messages = new List<Message>();
+        public List<Message> Messages { get { return messages; } }
+    }
+}
