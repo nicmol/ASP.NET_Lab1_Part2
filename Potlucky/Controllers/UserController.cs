59e7a8eebff2e7e7a8bb32c014fea9f829f803fe
Nicolette <molitorn@my.lanecc.edu>
2019-10-13 10:29:26 -0700

    add user and userList, test

---
+++ Potlucky/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Potlucky.Models;
+using System.Collections.Generic;
+
+namespace Potlucky.Controllers
+{
+    public class UserController: Controller
+    {
+        //Testin add user
+        User user;
+        public UserController()
+        {
+           if(UserList.Users.Count == 0)
+            {
+                user = new User()
+                {
+                    FirstName = "Nicolette",
+                    LastName = "Molitor",
+                    Email = "molitorn@my.lanecc.edu"
+                };
+                UserList.AddUser(user);
+            }
+        }
+
+
+
+        public IActionResult Index()
+        {
+            List<User> users = UserList.Users;
+            return View(users);
+        }
+
+
+    }
+}
--- Potlucky/Models/UserList.cs
+++ Potlucky/Models/UserList.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace Potlucky.Models
 {
-    public class UserList
+    public static class UserList
     {
+        private static List<User> users = new List<User>();
+        public static List<User> Users { get { return users; } }
+        public static void AddUser(User user)
+        {
+            users.Add(user);
+        }
+
     }
 }
