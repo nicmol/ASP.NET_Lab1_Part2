18fb1d301877878b88f411e5183e400cc0cca2c5
Nicolette <molitorn@my.lanecc.edu>
2019-10-20 15:32:37 -0700

    changed date format

--- Potlucky/Controllers/UserController.cs
+++ Potlucky/Controllers/UserController.cs
@@ -6,23 +6,8 @@ namespace Potlucky.Controllers
 {
     public class UserController: Controller
     {
-        //Testing add user
+        
         User user;
-        public UserController()
-        {
-           if(UserList.Users.Count == 0)
-            {
-                user = new User()
-                {
-                    FirstName = "Nicolette",
-                    LastName = "Molitor",
-                    Email = "molitorn@my.lanecc.edu"
-                };
-                UserList.AddUser(user);
-            }
-        }
-
-
 
         public IActionResult Index()
         {
